073a7d2024dc99d085ce66ced276e4e97aff8638
GNMmad <gnevado@yahoo.com>
2015-06-17 13:31:08 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions are able to cache the calculation of the inverse of a given matrix 'x'.
+## If the contents of a then matrix 'x' vector are not changing, and the inverse of 'x' 
+## is calculated and cached, it will not be recalculated, but looked up in the cache.
+## This functions will make use of scoping rules and global assignment with <<-.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix  <- function(x = matrix()) {
+  ## Creates a special "vector", which is really a list containing a function to:
+  ## set the value of the matrix 'x'
+  ## get the value of the matrix 'x'
+  ## set the value of the inverse of the matrix 'x'
+  ## get the value of the inverse of the matrix 'x'
+    m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## Get the cached value of the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    ## Inverse of 'x' is already cached, was previously calculated
+    message("getting cached data")
+    return(m)
+  }
+  ## Inverse of 'x' is not cached, has to be calculated and cached
+  ## Get matrix 'x'
+  data <- x$get()
+  ## Calculate inverse of matrix 'x'
+  m <- solve(data, ...)
+  ## Set and cache the value of the inverse of matrix 'x'
+  x$setinverse(m)
+  ## Return calculated inverse of matrix 'x'
+  m
 }
+
